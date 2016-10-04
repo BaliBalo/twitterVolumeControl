72a09a406cfcb9d06a69e1859914c096bacdba96
BaliBalo <balibalo@pause-geek.fr>
2016-10-05 01:24:51 +0200

    Match the new twitter video design

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 	
 	// ----- Options -----
 	var transitionTime = '.3s';
-	var controllerHeight = 80;
-	var trackHeight = 50;
+	var controllerSize = 80;
+	var trackSize = 68;
 	var trackWidth = 2;
 	var ballSize = 12;
 	var storageKey = 'tvc-volume';
@@ -12,14 +12,20 @@
 	var soundButton = document.querySelector('.sound-button');
 	if(!soundButton) return;
 	
-	var barTop = (controllerHeight - trackHeight) / 2;
+	var volume = localStorage.getItem(storageKey);
+	if(volume === null) volume = 1;
+	volume = Math.min(Math.max(+volume, 0), 1);
+	
+	var trackPos = (controllerSize - trackSize) / 2;
 	
 	soundButton.style.position = 'relative';
+	soundButton.style.transition = 'margin-right '+transitionTime;
 	var wrapper = document.createElement('div');
 	wrapper.style.position = 'absolute';
-	wrapper.style.width = '100%';
-	wrapper.style.height = controllerHeight+'px';
-	wrapper.style.bottom = '100%';
+	wrapper.style.top = '0';
+	wrapper.style.bottom = '5px';
+	wrapper.style.left = '100%';
+	wrapper.style.width = controllerSize + 'px';
 	wrapper.style.overflow = 'hidden';
 	wrapper.style.visibility = 'hidden';
 	wrapper.style.transition = 'visibility '+transitionTime;
@@ -27,13 +33,13 @@
 	bar.style.position = 'absolute';
 	bar.style.width = '100%';
 	bar.style.height = '100%';
-	bar.style.backgroundColor = 'rgba(0,0,0,.6)';
+	// bar.style.backgroundColor = 'rgba(0,0,0,.6)';
 	bar.style.backgroundImage = 'linear-gradient(white, white)';
 	bar.style.backgroundRepeat = 'no-repeat';
-	bar.style.backgroundSize = trackWidth + 'px ' + trackHeight + 'px';
+	bar.style.backgroundSize = trackSize + 'px ' + trackWidth + 'px';
 	bar.style.backgroundPosition = 'center';
 	bar.style.borderRadius = '5px 5px 0 0';
-	bar.style.transform = 'translateY(100%)';
+	bar.style.transform = 'translateX(-100%)';
 	bar.style.transition = 'transform '+transitionTime;
 	var ball = document.createElement('div');
 	ball.style.position = 'absolute';
@@ -41,8 +47,8 @@
 	ball.style.height = ballSize+'px';
 	ball.style.borderRadius = '50%';
 	ball.style.background = 'white';
-	ball.style.left = '50%';
-	ball.style.top = barTop + 'px';
+	ball.style.left = trackPos + 'px';
+	ball.style.top = '50%';
 	var hbs = ballSize / 2;
 	ball.style.margin = '-'+hbs+'px 0 0 -'+hbs+'px';
 	
@@ -52,33 +58,44 @@
 		if(!video) requestAnimationFrame(waitForVideo);
 		else videoReady();
 	}
+	updateVolume(volume);
 	waitForVideo();
 	
 	function videoReady() {
-		var stored = localStorage.getItem(storageKey);
-		if(stored === null) stored = 1;
-		stored = Math.min(Math.max(+stored, 0), 1);
-		setVolume(stored);
+		setVolume(volume);
+		video.addEventListener('volumechange', function() {
+			updateVolume(video.volume, video.muted);
+		});
 	}
 	
 	function setVolume(vol) {
-		ball.style.transform = 'translateY('+((1 - vol) * trackHeight)+'px)';
-		if(video) video.volume = vol;
+		volume = vol;
+		if(video) {
+			// video.muted = false;
+			video.volume = vol;
+		}
+	}
+	function updateVolume(vol, muted) {
+		// var pos = muted ? 0 : vol * trackSize;
+		var pos = vol * trackSize;
+		ball.style.transform = 'translateX('+pos+'px)';
 		localStorage.setItem(storageKey, vol);
 	}
 	
 	var hovered = false;
 	var mouseDown = false, preventClick = false;
-	var barPos = null;
+	var barScreenPos = null;
 	function showBar() {
 		if(!wrapper || !bar) return;
+		soundButton.style.marginRight = (15 + controllerSize) + 'px';
 		wrapper.style.visibility = 'visible';
-		bar.style.transform = 'translateY(0)';
+		bar.style.transform = 'translateX(0)';
 	}
 	function hideBar() {
 		if(!wrapper || !bar) return;
+		soundButton.style.marginRight = '15px';
 		wrapper.style.visibility = 'hidden';
-		bar.style.transform = 'translateY(100%)';
+		bar.style.transform = 'translateX(-100%)';
 	}
 	soundButton.addEventListener('mouseenter', function() {
 		hovered = true;
@@ -89,14 +106,14 @@
 		if(!mouseDown) hideBar();
 	});
 	function checkMouse(e) {
-		if(!barPos) return;
-		var posIn = e.clientY - barPos.top - barTop;
-		var result = 1 - Math.min(Math.max(posIn / trackHeight, 0), 1);
+		if(!barScreenPos) return;
+		var posIn = e.clientX - barScreenPos.left - trackPos;
+		var result = Math.min(Math.max(posIn / trackSize, 0), 1);
 		setVolume(result);
 	}
 	bar.addEventListener('mousedown', function(e) {
 		e.stopPropagation();
-		barPos = wrapper.getBoundingClientRect();
+		barScreenPos = wrapper.getBoundingClientRect();
 		mouseDown = true;
 		preventClick = true;
 		checkMouse(e);
@@ -108,12 +125,13 @@
 	document.addEventListener('mouseup', function(e) {
 		if(mouseDown) {
 			e.stopPropagation();
+			e.preventDefault();
 		} else {
 			preventClick = false;
 		}
 		mouseDown = false;
 		if(!hovered) hideBar();
-	});
+	}, true);
 	bar.addEventListener('click', function(e) {
 		if(preventClick)
 			e.stopPropagation();
